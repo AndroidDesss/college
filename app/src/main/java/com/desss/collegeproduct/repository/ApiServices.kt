b92d55068f317410ed6df22a61cbd2a3ab3433c6
Vishnu <vishnusadan21@gmail.com>
2024-01-14 11:37:21 +0530

    base commit

---
+++ app/src/main/java/com/desss/collegeproduct/repository/ApiServices.kt
@@ -0,0 +1,11 @@
+package com.desss.collegeproduct.repository
+
+import retrofit2.Call
+import retrofit2.http.*
+
+
+interface ApiServices {
+//    @GET("account/profile")
+//    fun getAccountProfile(@Url url: String?, ): Call<AccountProfileResponseModel?>?
+
+}
